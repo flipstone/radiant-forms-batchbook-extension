23ce842956c3f2025d851e57ed15742a06c2623e
Scott Conley <scranky@orangejello.local>
2010-10-21 17:24:29 -0400

    renamed base model class to fit new extension paradigm

---
+++ app/models/form_batchbook_contact.rb
@@ -0,0 +1,69 @@
+class FormBatchbookContact
+  attr_reader :config, :data
+  
+  def initialize(form, page)
+    @data   = page.data
+    @config = form.config[:batchbook]
+    # @body   = page.render_snippet(form)
+  end
+
+  def create
+    ApiCaller.add_contact( 
+      :account => "qre",
+      :token => "izSJrfcpFT"
+    )
+    
+    @success = true
+  rescue Exception => exception
+    @message = exception
+    @success = false
+  end
+  
+  def subject
+    hash_retrieve(@data, @config[:field]['subject']) || @config[:subject]
+  end
+  
+  def cc
+    @config[:cc]
+  end
+  
+  def success?
+    @success || false
+  end
+  
+  def message
+    @message || nil
+  end
+  
+  def files
+    res = []
+    data.each_value do |d|
+      res << d if StringIO === d or Tempfile === d
+    end
+    res
+  end
+  
+  def filesize_limit
+    config[:filesize_limit] || nil
+  end
+  
+  def headers
+    @headers = { 'Reply-To' => reply_to }
+    if sender
+      @headers['Return-Path'] = sender
+      @headers['Sender'] = sender
+    end
+  end
+  
+protected
+  
+  # takes object[value] || value and accesses the hash as hash[object][value] || hash[value]
+  def hash_retrieve(hash, array)
+    data = array.gsub("[","|").gsub("]","").split("|") rescue nil
+    
+    result = false
+    result = hash.fetch(data[0]) unless data.nil?
+    result = result.fetch(data[1]) if !data.nil? and data[1]
+  end
+  
+end
